b55ab5d6858d31c9c7fa2286c6e29e5fbda48db4
aziz jlassi <azizjlassi498@gmail.com>
2024-05-07 22:40:36 +0100

    Adding CI/CD Pipeline

--- LittleLemonApi/apps.py
+++ LittleLemonApi/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class LittlelemonapiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "LittleLemonApi"
-
-    def ready(self) -> None:
-        import LittleLemonApi.signals 
